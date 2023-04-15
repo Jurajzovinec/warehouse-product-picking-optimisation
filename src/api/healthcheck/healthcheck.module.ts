0e9bbb963de4512361f0393d79403d61c0c883f8
Jurajzovinec <juraj.zovinecc@gmail.com>
2023-04-15 11:10:39 +0200

    chore: remove healthCheck

--- src/api/healthcheck/healthcheck.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthCheckService } from './HealthCheck.service';
-import { HealthCheckController } from './HealthCheck.controller';
-
-@Module({
-  controllers: [HealthCheckController],
-  providers: [HealthCheckService]
-})
-export class HealthCheckModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { BoxPiModule } from '../providers/boxpi/boxpi.module';
-import { HealthCheckModule } from '../api/HealthCheck/HealthCheck.module';
 import { OptimizerModule } from '../api/optimizer/optimizer.module';
 import { ConfigModule } from '@nestjs/config';
 import boxpiConfig from '../config/boxpi.config';
@@ -12,7 +11,6 @@ import boxpiConfig from '../config/boxpi.config';
       cache: true
     }),
     BoxPiModule,
-    HealthCheckModule,
     OptimizerModule
   ]
 })
